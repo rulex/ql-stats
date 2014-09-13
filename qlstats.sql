6323618a56421843e7ea93dadfecac11c230ac4b
Lars Wikberg <lars.wikberg@gmail.com>
2014-09-13 12:53:46 +0300

    formatting

--- qlstats.sql
+++ qlstats.sql
@@ -151,37 +151,16 @@ CREATE TABLE `GameTag` (
 ) CHARACTER SET ascii ENGINE=MyISAM;
 
 DROP TABLE IF EXISTS Race;
-CREATE TABLE Race (MODE int not null, MAP_ID integer not null, SCORE integer not null, PLAYER_ID integer not null, RANK integer not null, GAME_TIMESTAMP integer not null, GAME_ID integer) CHARACTER SET ascii engine=MyISAM;
+CREATE TABLE Race (
+	MODE int not null,
+	MAP_ID integer not null,
+	SCORE integer not null,
+	PLAYER_ID integer not null,
+	RANK integer not null,
+	GAME_TIMESTAMP integer not null,
+	GAME_ID integer
+) CHARACTER SET ascii engine=MyISAM;
+
 CREATE INDEX IX_RaceMap on Race (MAP_ID, MODE, RANK);
 CREATE INDEX IX_RacePlayer on Race (PLAYER_ID, MODE);
 
-drop view if exists Games;
-/*
-create view Games as 
-select g.*,m.NAME as MAP 
-from Game g 
-inner join Map m on m.ID=g.MAP_ID;
-*/
-
-drop view if exists Players;
-/*
-create view Players as
-select g.PUBLIC_ID,gp.*,p.NAME as PLAYER_NICK,c.NAME as PLAYER_CLAN,p.COUNTRY as PLAYER_COUNTRY
-from GamePlayer gp
-inner join Game g on g.ID=gp.GAME_ID
-inner join Player p on p.ID=gp.PLAYER_ID
-inner join Clan c on c.ID=gp.CLAN_ID;
-*/
-
-/*
--- remove duplicate MAP records (same NAME with different ID)
-
-drop table if exists MapFix;
-create table if not exists MapFix as (select m1.ID `OLD_ID`, min(m2.ID) `NEW_ID` from Map m1, Map m2 where m2.NAME=m1.NAME group by m1.NAME);
-create unique index IX_ID on MapFix(OLD_ID);
-update Game g, MapFix f set g.MAP_ID=f.NEW_ID where f.OLD_ID=g.MAP_ID;
-delete from Race where MAP_ID not in (select NEW_ID from MapFix);
-delete from Map where ID not in (select NEW_ID from MapFix);
-create unique index IX_Map_NAME on Map(NAME);
-drop table MapFix;
-*/
